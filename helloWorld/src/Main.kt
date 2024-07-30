46048aa245e4c7c7773599df26686043273a07de
DevFelipreis <lfreis.contato@gmail.com>
2024-07-30 14:20:11 -0300

    firtscommit

--- helloWorld/src/Main.kt
+++ helloWorld/src/Main.kt
@@ -1,11 +1,24 @@
 fun main() {
-    var nome = "Felipe"
-    val idade = 31 //constante
-    println("Hello, $nome, você tem $idade anos")
 
+    val price: Double = 31.90 // 64-bit ponto flutuante(floating point)
+    val desconto: Float = 31.90f // 32-bit
+    val byte: Byte = 8 //8-bit
+    val short: Short = 16 // 16-bit
+    val age: Int = 31 // 32-bit (99% do tempo)
+    val long: Long = 22222222222222 // 64-bit
+    val product: String  = "Imac"
 
-    nome = "Luiz Felipe"
-    println("Hello, $nome, você tem $idade anos")
+    val nomes = listOf("Felipe", "Eduardo")
+    val idades = listOf(31, 1)
 
+    for (i in nomes.indices) {
 
+        if(idades[i] > 1){
+            println("${nomes[i]} tem ${idades[i]} anos")
+        }else{
+            println("${nomes[i]} tem ${idades[i]} ano")
+        }
+    }
+
+    println(nomes::class)
 }
