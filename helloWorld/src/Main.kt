782b3194125e56de9b592022fd1769704f448be7
DevFelipreis <lfreis.contato@gmail.com>
2024-07-30 13:50:09 -0300

    firtscommit

---
+++ helloWorld/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var nome = "Felipe"
+    val idade = 31 //constante
+    println("Hello, $nome, você tem $idade anos")
+
+
+    nome = "Luiz Felipe"
+    println("Hello, $nome, você tem $idade anos")
+
+
+}
